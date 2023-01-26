b4564dced222bbc2d669afc99f556486312020ee
Haf <haf@protonmail.ch>
2023-01-26 08:43:05 +0100

    published it

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.20"
     kotlin("plugin.serialization") version "1.7.20"
+    id("maven-publish")
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("signing")
 }
 
 group = "xyz.haff"
 version = "0.1.0"
+
+tasks.wrapper {
+    gradleVersion = "7.4"
+}
 
 repositories {
     mavenCentral()
@@ -29,5 +36,63 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                packaging = "jar"
+                name.set(project.name)
+                description.set("Simple OAuth2 client library")
+
+                url.set("https://github.com/huuff/kotlin-oauth-client")
+                scm {
+                    connection.set("scm:git:git://github.com/huuff/kotlin-oauth-client.git")
+                    developerConnection.set("scm:git:git@github.com:huuff/kotlin-oauth-client.git")
+                    url.set("https://github.com/huuff/kotlin-oauth-client/tree/master")
+                }
+
+                licenses {
+                    license {
+                        name.set("WTFPL - Do What The Fuck You Want To Public License")
+                        url.set("http://www.wtfpl.net")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("Francisco Sánchez")
+                        email.set("haf@protonmail.ch")
+                        organizationUrl.set("https://github.com/huuff")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(properties["sonatype.user"] as String)
+            password.set(properties["sonatype.password"] as String)
+        }
+    }
 }
